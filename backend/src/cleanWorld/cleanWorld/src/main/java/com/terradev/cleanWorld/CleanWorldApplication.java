8205797b2602a2523aa229ff7b4479fc53655ca9
marcos <marcoszabalarodriguez@gmail.com>
2025-12-01 10:02:37 +0100

    Feat: Added .gitignore and Springboot

---
+++ backend/src/cleanWorld/cleanWorld/src/main/java/com/terradev/cleanWorld/CleanWorldApplication.java
@@ -0,0 +1,13 @@
+package com.terradev.cleanWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanWorldApplication.class, args);
+	}
+
+}
