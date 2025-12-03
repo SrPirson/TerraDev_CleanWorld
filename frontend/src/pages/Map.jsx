bc70fad68ab839a46ecd66cfe9aacee13abbeae8
Sergio-Benitez <sergio.benitez.ramirez@gmail.com>
2025-12-03 13:08:45 +0100

    Update: Change Map page including new RecyclingMenu component and styles

--- frontend/src/pages/Map.jsx
+++ frontend/src/pages/Map.jsx
@@ -1,11 +1,15 @@
 import NavBar from '../components/NavBar.jsx';
 import Mapa from '../components/Map.jsx';
+import RecyclingMenu from '../components/RecyclingMenu.jsx';
 
 export default function MapaPage() {
   return (
-	<div className="min-h-screen flex flex-col">
-	  <NavBar />
-      <Mapa />
+    <div className="h-screen flex flex-col">
+      <NavBar />
+      <div className="flex-1 relative">
+        <Mapa />
+        <RecyclingMenu />
+      </div>
     </div>
   );
 }
